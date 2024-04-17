baf552012ee62598caf2a8ce0848a59595a9a708
riiy <riiyzhou@gmail.com>
2024-04-17 17:00:57 +0800

    update env to upper

--- src/emtl/tests/test_core.py
+++ src/emtl/tests/test_core.py
@@ -13,4 +13,4 @@ def test_emt_empty():
 
 
 def test_login():
-    assert len(login(os.getenv("em_username", ""), os.getenv("em_password", ""))) == len("b91d8012-b70b-4265-bb5d-f79442531017")
+    assert len(login(os.getenv("EM_USERNAME", ""), os.getenv("EM_PASSWORD", ""))) == len("b91d8012-b70b-4265-bb5d-f79442531017")
