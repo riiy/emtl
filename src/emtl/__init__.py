cfce27a3174ac5c0763001b3192f7b84e2e7b5cc
riiy <riiyzhou@gmail.com>
2024-04-25 11:22:00 +0800

    add methods to __init__

--- src/emtl/__init__.py
+++ src/emtl/__init__.py
@@ -3,9 +3,19 @@ __version__ = "0.1.5"
 from .core import emt
 from .core import login
 from .core import query_asset_and_position
+from .core import query_funds_flow
+from .core import query_history_orders
+from .core import query_history_trades
+from .core import query_orders
+from .core import query_trades
 
 __all__ = [
     "emt",
     "login",
     "query_asset_and_position",
+    "query_funds_flow",
+    "query_history_orders",
+    "query_history_trades",
+    "query_orders",
+    "query_trades",
 ]
